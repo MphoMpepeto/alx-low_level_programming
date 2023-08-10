b85154cc38798e466d026f69f67e236c459b5970
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-10 04:19:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - serves as an entry point of the program.
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
