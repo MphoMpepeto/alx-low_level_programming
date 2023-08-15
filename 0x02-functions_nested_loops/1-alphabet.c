5b9467c2a3368debb69ba49c120c90a9d634d27e
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-15 10:00:42 -0700

    header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Description: 'a function that prints out lowercase lettrs of the alphabet'
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	putchar(
+
