e1f08f5217ae7436c74a1707d776ce84da2d9fd3
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-17 06:50:10 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints numbers 0-14 ten times
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
