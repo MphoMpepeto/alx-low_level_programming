0fcfdc9601ff088b7ea109942576d728f2b18abf
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-22 14:03:48 -0700

    prints specific elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,15 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - check the code for
- *
+ * print_array - Prints a specific number of elements of an array
+ * @a: an array of type integer
+ * @n: the number of elements in an array
  * Return: Always 0.
  */
 
 void print_array(int *a, int n)
 
-{ 
-	int a;
-	int n;
+{
+	int e;
 
+	for (e = 0; e < n; e++)
+	{
+		printf("%d", a[e]);
+		if (e != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
