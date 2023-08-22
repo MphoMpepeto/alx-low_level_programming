031bdbdd17a387005eec3aa2e133d36ee1e688f9
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-22 14:12:37 -0700

    copies a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * char*_strcpy - check the code
- *
+ * *_strcpy - A function that copies a string
+ * @src: the string
+ * @dest: A buffer
  * Return: Always 0.
  */
 
 char *_strcpy(char *dest, char *src)
 
 {
-	char *dest;
-	char *src;
+	int f;
 
+	for (f = 0; src[f] != '\0'; f++)
+	{
+		dest[f] = src[f];
+	}
+	dest[f++] = '\0';
+	return (dest);
 }
