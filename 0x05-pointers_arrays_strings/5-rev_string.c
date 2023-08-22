73ad4ce23f7ad5d0a234500d00e1c784bb5179fe
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-22 13:31:21 -0700

    printing a string in reverse using arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string - check the code
- *
+ * rev_string - a function that reverses a string
+ * @s: represents the string
  * Return: Always 0.
  */
 
 void rev_string(char *s)
 
 {
-	char s;
+	int length = 0, index = 0;
+	char dis;
 
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= length / 2; index--)
+	{
+		dis = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = dis;
+	}
 }
