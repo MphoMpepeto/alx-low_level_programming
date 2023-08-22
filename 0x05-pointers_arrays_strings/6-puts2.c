8f4a74b013e75ebfe45296122dae9b06870046e6
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-22 13:39:22 -0700

    prints alternating characters of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,26 @@
 #include "main.h"
 
 /**
- * puts2 - check the code
- *
+ * puts2 - a function that prints alternating characters of a string
+ * @str: represents the srting to be printed
  * Return: Always 0.
  */
 
 void puts2(char *str)
 
 {
-	char str;
+	int a;
+	int b;
 
+	b = 0;
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
 }
