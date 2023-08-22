be1fcfaffb2de0beec8c0061c1f76ff9c7fd50b4
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-22 12:11:53 -0700

    integer swapping function

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - check the code
- *
+ * swap_int - a function that swaps the value of two integers
+ * @a: the first integer that may be swapped.
+ * @b: the second integer that the first will be swapped with.
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
-	int n;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
