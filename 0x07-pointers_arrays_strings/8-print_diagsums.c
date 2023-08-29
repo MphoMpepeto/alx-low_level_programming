8bdf70c06976ab85aa34d5e977dce53d8cbf071e
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-08-29 04:50:27 -0700

    print diag sums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagsums - a function that locates a substring
+ * @a: a pointer to the variable to be printed
+ * @size: size of the array
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int w;
+	int sum_a = 0;
+	int sum_b = 0;
+
+	for (w = 0; w < size; w++)
+	{
+		sum_a += a[w];
+		a += size;
+	}
+	a += size;
+
+	for (w = 0; w < size; w++)
+	{
+		sum_b += a[w];
+		a += size;
+	}
+	printf("%d, %d\n", sum_a, sum_b);
+}
