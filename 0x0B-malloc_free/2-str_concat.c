07d431370a1d8698eabd90d74e8aa6329eb8059d
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-05 07:40:48 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,23 @@ char *str_concat(char *s1, char *s2)
 	int s1_len = 0;
 	int s2_len = 0;
 	int i;
-	char *s;
+	char *h;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
-		s1++;
+		s1_len++;
 	for (i = 0; s2[i] != '\0'; i++)
-		s2++;
-	s = malloc(sizeof(char) * (s1_len + s2_len + 1));
+		s2_len++;
+	h = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
-	if (s == NULL)
+	if (h == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
+		h[i] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
-		s[s1_len + i] = s2[i];
-	return (s);
+		h[s1_len + i] = s2[i];
+	return (h);
 }
