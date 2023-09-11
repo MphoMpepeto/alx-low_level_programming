b4caaeb8b4098759b6a3bc9ce18c365fe12cd969
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-11 11:23:02 -0700

    function like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * ABS(x) - A header file that computes if x is an absolute value.
+ */
+
+#endif
