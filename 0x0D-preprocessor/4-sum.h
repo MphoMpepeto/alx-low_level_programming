29a511270fc432fbbe0c327ee4482e2be3d9a560
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-11 11:29:14 -0700

    sum macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_SUM_H
+#define FUNCTION_LIKE_MACRO_SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+/**
+ * SUM(x, y) - A header file that computes the sum of x and y.
+ */
+
+#endif
