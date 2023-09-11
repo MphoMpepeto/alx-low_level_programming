f73cc9e4c2f0b359caf41023dda9f0b15e6fbbe1
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-11 11:53:10 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - a struct with information about a dog
+ * @name: a pointer to the name of the dog
+ * @age: the age of the dog
+ * @owner: a pointer to the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
