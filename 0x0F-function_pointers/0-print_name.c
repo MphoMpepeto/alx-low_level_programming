07ae0bbe684819e9a7fb1bcf52fd2f3ff6eccc13
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-13 11:13:16 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: a pointer to a var
+ * @f: a function pointer that returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
