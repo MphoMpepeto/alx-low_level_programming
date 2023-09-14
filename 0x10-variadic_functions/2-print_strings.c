7183257664744573419c611e37a50a68c04cd85f
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-14 03:32:28 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,3 +9,31 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	const char *string;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, const char *);
+
+		if (string == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", string);
+		}
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
+
