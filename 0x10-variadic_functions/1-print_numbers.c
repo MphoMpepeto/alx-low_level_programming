16a4310c9cc2e3249d7f66430cb497fa4508ac1a
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-14 03:22:40 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,3 +9,22 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	int x;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d", x);
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
