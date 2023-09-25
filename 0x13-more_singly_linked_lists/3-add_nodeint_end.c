a498a589a2f7e30c65f260ffe7effab3d3eafc1e
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-25 14:34:39 -0700

    add element at the end of a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,9 +3,40 @@
 /**
  * add_nodeint_end - a function that adds a new node at the end of a list
  * @head: a double pointer to the list
- * @n: 
+ * @n: element to be added at the end of list
  *
  * Return: the address of the new element or NULL if failed
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_end;
+	listint_t *tmp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_end = malloc(sizeof(listint_t));
+
+	if (new_end == NULL)
+	{
+		return (NULL);
+	}
+	new_end->n = n;
+	new_end->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_end;
+		return (new_end);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new_end;
+	return (new_end);
+}
