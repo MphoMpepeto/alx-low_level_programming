5ca21e28e72c6565f67fb84896416311362e9a33
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-25 13:39:15 -0700

    number of elements in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,24 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of all the elements of a list
- * @h: a pointer to the list
+ * listint_len - a function that returns the number of elements of a list
+ * @h: a pointer to the head of the list
  *
  * Return: the number of nodes
  */
 
 size_t listint_len(const listint_t *h)
+{
+	size_t nodes;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (nodes = 0; h != NULL; nodes++)
+	{
+		h = h->next;
+	}
+	return (nodes);
+}
+
