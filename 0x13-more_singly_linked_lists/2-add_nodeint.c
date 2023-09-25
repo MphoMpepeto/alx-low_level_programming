eb807ade0c7050a52b1d4eaa1dae07e41c9e997d
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-25 13:53:15 -0700

    add new element to list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,9 +3,35 @@
 /**
  * add_nodeint - a function that prints all the elements of a list
  * @head: a double pointer to the list
- * @n: 
+ * @n: new element to be created
  *
  * Return: the address of the new element or NULL if failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_element;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	new_element = malloc(sizeof(listint_t));
+
+	if (new_element == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		new_element->next = NULL;
+	}
+	else
+	{
+		new_element->next = *head;
+	}
+	new_element->n = n;
+	*head = new_element;
+	return (0);
+}
