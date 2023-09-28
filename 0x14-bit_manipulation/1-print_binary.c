59a8652251102934cc5c3f554fce504edfab3860
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-09-28 10:54:30 -0700

    print binary numbers

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,3 +9,29 @@
 
 void print_binary(unsigned long int n)
 {
+	int print_f = 0;
+	int bit_pos;
+
+	if (n == 0)
+	{
+		printf("0");
+	}
+
+	bit_pos = sizeof(unsigned long int) * 8 - 1;
+
+	while (bit_pos >= 0)
+	{
+		unsigned long int x = 1UL << bit_pos;
+
+		if ((n & x) != 0)
+		{
+			_putchar('1');
+			print_f = 1;
+		}
+		else if (print_f)
+		{
+			_putchar('0');
+		}
+		bit_pos--;
+	}
+}
