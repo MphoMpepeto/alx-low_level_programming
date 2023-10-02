8b75fb96d8ba9e9453c351ec02c18d1975ede625
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-10-02 07:58:36 -0700

    copy file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFF_SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,5 +13,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void exit_err(int exit_code, const char *exit_msg);
+int main(int argc, char *argv[]);
 
 #endif
