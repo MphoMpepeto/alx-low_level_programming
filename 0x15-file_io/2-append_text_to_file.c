f71cb636b00f4570b8cf0ad056e0b37fda52532f
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-10-02 06:12:57 -0700

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  * append_text_to_file - a function that appends text at the end of file
- * @filename: the name of the file 
+ * @filename: the name of the file
  * @text_content: a NULL terminated string to add at the end of file
  *
  * Return: 1 on success, -1 on failure
@@ -10,3 +10,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file_append;
+	ssize_t data_add;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_append = open(filename, O_WRONLY | O_APPEND);
+	if (file_append == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		data_add = write(file_append, text_content, strlen(text_content));
+		if (data_add == -1)
+		{
+			close(file_append);
+			return (-1);
+		}
+	}
+
+	close(file_append);
+	return (1);
+}
