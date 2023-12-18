4f602c359d4f7b6b1d7140012ac205a0c0421ad5
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-12-18 09:55:45 -0800

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,4 +10,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fl, new_letters, rdwr;
 
+	if (!filename)
+		return (0);
+	fl = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fl == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (new_letters = 0; text_content[new_letters]; new_letters++)
+		;
+	rdwr = write(fl, text_content, new_letters);
+	if (rdwr == -1)
+		return (-1);
+	close(fl);
+	return (1);
+}
