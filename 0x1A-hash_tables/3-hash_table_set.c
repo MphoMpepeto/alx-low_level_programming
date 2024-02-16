75da08b7a94e5ad05763f9175e8899169eba34a8
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-02-16 13:08:40 -0800

    hash tables set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,4 +9,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new;
+	char *cpy_value;
+	unsigned long int idx, i;
 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	cpy_value = strdup(value);
+	if (cpy_value == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = cpy_value;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(cpy_value);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = cpy_value;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
+}
