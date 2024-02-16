59af9cd58057900869c6abfea26da7e824077975
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2024-02-16 13:14:31 -0800

    hash tables get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,10 +3,26 @@
 /**
  * hash_table_get -  a function that retrieves a value associated with a key
  * @ht:  hash table you want to look into
- * 2key: the key you are looking for
+ * key: the key you are looking for
+ *
  * Return: NULL or the value associated with the element
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
+	unsigned long int idx;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (idx >= ht->size)
+		return (NULL);
+
+	node = ht->array[idx];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
